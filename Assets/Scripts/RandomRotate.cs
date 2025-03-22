7c224286a33fd1525f85a1dae0c0eb1e78d77d1d
Shuhei Suzuki <raspbell.caf2@gmail.com>
2025-03-23 00:03:23 +0900

    くるくるまるまる

--- Assets/Scripts/RandomRotate.cs
+++ Assets/Scripts/RandomRotate.cs
@@ -3,13 +3,27 @@ using DG.Tweening;
 
 public class RandomRotate : MonoBehaviour
 {
+    [SerializeField] float tweenDuration = 1f;
+    [SerializeField] float rotationAngle = 180f;
+    private Vector3 randomVector;
+    public static Vector3 RandomVector3()
+    {
+        Vector3 v = Vector3.zero;
+        v.x = Random.Range(-1.0f, 1.0f);
+        v.y = Random.Range(-1.0f, 1.0f);
+        v.z = Random.Range(-1.0f, 1.0f);
+        return v.normalized;
+    }
     void Start()
     {
+        randomVector = RandomVector3();
         Rotate();
     }
 
     void Rotate()
     {
-        transform.DORotate(new Vector3(Random.Range(0, 720), Random.Range(0, 720), Random.Range(0, 720)), 3f).OnComplete(() => Rotate());
+        transform.DORotate(Vector3.Cross(Vector3.one * rotationAngle, randomVector), tweenDuration, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Incremental);
     }
 }
--- Assets/Scripts/SourceGenerator.cs
+++ Assets/Scripts/SourceGenerator.cs
@@ -1,18 +1,21 @@
 using System.Collections.Generic;
 using Apple.PHASE;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class SourceGenerator : MonoBehaviour
 {
     // AudioClip のリスト
     public List<AudioClip> audioClips;
-    // 配置するスピーカのPrefab
-    public GameObject speakerPrefab;
+    // 配置するスピーカのPrefab(殻)
+    public GameObject speakerShellPrefab;
+    // 配置するスピーカのPrefab(核)
+    public GameObject speakerCorePrefab;
     // カメラ（中心となるオブジェクト）
     public GameObject camera;
     // 配置する半径（任意の値に調整可能）
     public float radius = 1f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,23 +29,36 @@ public class SourceGenerator : MonoBehaviour
             // 各スピーカを均等な角度で配置 (角度は度単位)
             float angle = i * (360f / count);
             float angleRad = angle * Mathf.Deg2Rad;
-            
+
             // カメラの位置を中心に、指定した半径で計算
             Vector3 pos = camera.transform.position + new Vector3(Mathf.Cos(angleRad) * radius, 1f, Mathf.Sin(angleRad) * radius);
             // スピーカがカメラの方向を向くように回転を設定
             Quaternion rotation = Quaternion.LookRotation(camera.transform.position - pos);
-            
+
             // スピーカの生成
-            GameObject speaker = Instantiate(speakerPrefab, pos, rotation);
-            speaker.transform.parent = this.transform;
-            
+            GameObject speakerShell = Instantiate(speakerShellPrefab, pos, rotation);
+            speakerShell.transform.SetParent(transform, true);
+
+            // スピーカの生成
+            GameObject speakerCore = Instantiate(speakerCorePrefab, pos, rotation);
+            speakerCore.transform.SetParent(transform, true);
+
+            // speakerCore の PositionConstraint に speakerShell をソースとして追加
+            PositionConstraint posConstraint = speakerCore.GetComponent<PositionConstraint>();
+            ConstraintSource source = new ConstraintSource();
+            source.sourceTransform = speakerShell.transform;
+            source.weight = 1.0f;
+            posConstraint.AddSource(source);
+            posConstraint.constraintActive = true;
+
+
             // AudioClip に対応する DynamicSoundEvent の初期化
-            DynamicSoundEvent dynamicSoundEvent = speaker.AddComponent<DynamicSoundEvent>();
+            DynamicSoundEvent dynamicSoundEvent = speakerShell.AddComponent<DynamicSoundEvent>();
             Debug.Log(audioClips[i].name);
             dynamicSoundEvent.Init(audioClips[i]);
-            
+
             // gain 設定
-            PHASESource phaseSource = speaker.GetComponent<PHASESource>();
+            PHASESource phaseSource = speakerShell.GetComponent<PHASESource>();
             phaseSource.SetGain(gain);
         }
     }

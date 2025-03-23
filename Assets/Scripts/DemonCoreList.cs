3ed3ecccbe341c4edb65497377b713d1b37bc098
SHIN <106070536+FRESH-SHIN@users.noreply.github.com>
2025-03-23 12:58:33 +0900

    add: core color

---
+++ Assets/Scripts/DemonCoreList.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonList : MonoBehaviour
+{
+    [SerializeField] public List<Material> materials;
+    [SerializeField] public List<GameObject> objects;
+    static public int index = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (var obj in objects)
+        {
+            MeshRenderer mesh = obj.GetComponent<MeshRenderer>();
+            mesh.materials = new Material[]{ materials[index]};
+        }
+        index = index + 1;
+        index = index % materials.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
